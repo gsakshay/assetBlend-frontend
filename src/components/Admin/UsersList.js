48881dd15bdd3aed679316f4eed3245321556b4f
Deep Prajapati <prajapati.de@northeastern.edu>
2023-12-10 12:31:58 -0500

    Eddited AdvisorList & UserList

--- src/components/Admin/UsersList.js
+++ src/components/Admin/UsersList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,32 +8,66 @@ import {
   TableRow,
   Paper,
   Typography,
+  Modal,
+  Box,
+  Button,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function AdminPage() {
-  // Sample data - replace this with your actual data
-  const users = [
-    { id: 1, name: "1. Firstname | Lastname | Email", stockCount: 15, advisor: "Advisor A" },
-    { id: 2, name: "2. Firstname | Lastname | Email", stockCount: 20, advisor: "Advisor B" },
-    { id: 3, name: "3. Firstname | Lastname | Email", stockCount: 12, advisor: "Advisor C" },
-    { id: 4, name: "4. Firstname | Lastname | Email", stockCount: 8, advisor: "Advisor D" },
-    { id: 5, name: "5. Firstname | Lastname | Email", stockCount: 10, advisor: "Advisor E" },
-    { id: 6, name: "6. Firstname | Lastname | Email", stockCount: 5, advisor: "Advisor A" },
-    { id: 7, name: "7. Firstname | Lastname | Email", stockCount: 18, advisor: "Advisor B" },
-    { id: 8, name: "8. Firstname | Lastname | Email", stockCount: 22, advisor: "Advisor C" },
-    { id: 9, name: "9. Firstname | Lastname | Email", stockCount: 9, advisor: "Advisor D" },
-    { id: 10, name: "10. Firstname | Lastname | Email", stockCount: 7, advisor: "Advisor F" },
-    { id: 11, name: "11. Firstname | Lastname | Email", stockCount: 13, advisor: "Advisor K" },
-    { id: 12, name: "12. Firstname | Lastname | Email", stockCount: 19, advisor: "Advisor L" },
-    { id: 13, name: "13. Firstname | Lastname | Email", stockCount: 11, advisor: "Advisor M" },
-    { id: 14, name: "14. Firstname | Lastname | Email", stockCount: 14, advisor: "Advisor N" },
-    { id: 15, name: "15. Firstname | Lastname | Email", stockCount: 6, advisor: "Advisor O" },
-    { id: 16, name: "16. Firstname | Lastname | Email", stockCount: 21, advisor: "Advisor P" },
-    { id: 17, name: "17. Firstname | Lastname | Email", stockCount: 17, advisor: "Advisor Q" },
-    { id: 18, name: "18. Firstname | Lastname | Email", stockCount: 4, advisor: "Advisor R" },
-    { id: 19, name: "19. Firstname | Lastname | Email", stockCount: 3, advisor: "Advisor S" },
-    { id: 20, name: "20. Firstname | Lastname | Email", stockCount: 16, advisor: "Advisor T" },
+  const initialUsers = [
+    { id: 1, name: "User 1", stockCount: 15, advisor: "Advisor A" },
+    { id: 2, name: "User 2", stockCount: 20, advisor: "Advisor B" },
+    { id: 3, name: "User 3", stockCount: 12, advisor: "Advisor C" },
+    { id: 4, name: "User 4", stockCount: 8, advisor: "Advisor D" },
+    { id: 5, name: "User 5", stockCount: 10, advisor: "Advisor E" },
+    { id: 6, name: "User 6", stockCount: 5, advisor: "Advisor A" },
+    { id: 7, name: "User 7", stockCount: 18, advisor: "Advisor B" },
+    { id: 8, name: "User 8", stockCount: 22, advisor: "Advisor C" },
+    { id: 9, name: "User 9", stockCount: 9, advisor: "Advisor D" },
+    { id: 10, name: "User 10", stockCount: 7, advisor: "Advisor F" },
+    { id: 11, name: "User 11", stockCount: 13, advisor: "Advisor K" },
+    { id: 12, name: "User 12", stockCount: 19, advisor: "Advisor L" },
+    { id: 13, name: "User 13", stockCount: 11, advisor: "Advisor M" },
+    { id: 14, name: "User 14", stockCount: 14, advisor: "Advisor N" },
+    { id: 15, name: "User 15", stockCount: 6, advisor: "Advisor O" },
+    { id: 16, name: "User 16", stockCount: 21, advisor: "Advisor P" },
+    { id: 17, name: "User 17", stockCount: 17, advisor: "Advisor Q" },
+    { id: 18, name: "User 18", stockCount: 4, advisor: "Advisor R" },
+    { id: 19, name: "User 19", stockCount: 3, advisor: "Advisor S" },
+    { id: 20, name: "User 20", stockCount: 16, advisor: "Advisor T" },
   ];
+
+  const [users, setUsers] = useState(initialUsers);
+  const [open, setOpen] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+
+  const handleOpen = (userId) => {
+    setOpen(true);
+    setSelectedUserId(userId);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const deleteUser = () => {
+    const updatedUsers = users.filter((user) => user.id !== selectedUserId);
+    setUsers(updatedUsers);
+    handleClose();
+  };
+
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -47,6 +81,7 @@ function AdminPage() {
               <TableCell>User Name</TableCell>
               <TableCell align="right">Number of Stocks</TableCell>
               <TableCell align="right">Advisor</TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -57,11 +92,46 @@ function AdminPage() {
                 </TableCell>
                 <TableCell align="right">{user.stockCount}</TableCell>
                 <TableCell align="right">{user.advisor}</TableCell>
+                <TableCell align="right">
+                  <DeleteIcon
+                    style={{ color: "red", cursor: "pointer" }}
+                    onClick={() => handleOpen(user.id)}
+                  />
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+      {/* Confirmation Modal */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="delete-confirmation-title"
+        aria-describedby="delete-confirmation-description"
+      >
+        <Box sx={modalStyle}>
+          <Typography
+            id="delete-confirmation-title"
+            variant="h6"
+            component="h2"
+          >
+            Confirm Deletion
+          </Typography>
+          <Typography id="delete-confirmation-description" sx={{ mt: 2 }}>
+            Are you sure you want to delete this user?
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 3 }}>
+            <Button sx={{ mr: 1 }} onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="contained" color="error" onClick={deleteUser}>
+              Delete
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </Paper>
   );
 }
--- src/components/advisor/AddAdvisor.js
+++ src/components/advisor/AddAdvisor.js
@@ -1,54 +1,109 @@
-import { Grid } from "@mui/material"
-import React from "react"
-import { useState } from 'react';
-import Paper from "@mui/material/Paper"
-import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button'; // Import Button component
-import Dialog from '@mui/material/Dialog'; // Import Dialog component
-import DialogTitle from '@mui/material/DialogTitle'; // Import DialogTitle component
-import DialogContent from '@mui/material/DialogContent'; // Import DialogContent component
-import TextField from '@mui/material/TextField'; // Import TextField component
-import Title from "../Title"
+import { Grid } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import Paper from "@mui/material/Paper";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button"; // Import Button component
+import Dialog from "@mui/material/Dialog"; // Import Dialog component
+import DialogTitle from "@mui/material/DialogTitle"; // Import DialogTitle component
+import DialogContent from "@mui/material/DialogContent"; // Import DialogContent component
+import TextField from "@mui/material/TextField"; // Import TextField component
+import Title from "../Title";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+
+const advisorsList = [
+  "Advisor 1",
+  "Advisor 2",
+  "Advisor 3",
+  "Advisor 4",
+  "Advisor 5",
+  "Advisor 6",
+  // Add more advisors as needed
+];
+
+const NoAdvisorFoundPage = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">No Advisor Found</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Typography variant="h4" align="center" style={{ marginTop: "50px" }}>
+          No advisor found.
+        </Typography>
+        {/* Add your content here */}
+      </Container>
+    </div>
+  );
+};
 
 function AddAdvisor() {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-    const handleAddAdvisorClick = () => {
-        setIsDialogOpen(true);
-    };
+  const [searchTerm, setSearchTerm] = useState("");
+  const [visibleAdvisors, setVisibleAdvisors] = useState(5);
 
-    const handleCloseDialog = () => {
-        setIsDialogOpen(false);
-    };
-	return (
-		<Grid container spacing={3}>
-            <Grid item xs={12} md={12} align="center" style={{ verticalAlign: 'middle' }}>
-                        <Paper
-                        sx={{
-                            p: 1,
-                        }}
-                        >
-                            <Title>No Advisor Found</Title>
-                            <Button variant="outlined" onClick={handleAddAdvisorClick}>
-                               <AddIcon/> Choose Advisor
-                            </Button>
-                        
-                        </Paper>
-                    </Grid>
+  const filteredAdvisors = advisorsList.filter((advisor) =>
+    advisor.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-                    <Dialog open={isDialogOpen} onClose={handleCloseDialog} align="center" style={{ verticalAlign: 'middle' }}>
-                        <DialogTitle>Add Advisor</DialogTitle>
-                        <DialogContent>
-                        {/* customize the content of the dialog here */}
-                        <TextField label="Search Advisor" fullWidth style={{ marginTop: '5px' }}/>
-                        {/* Add other components as needed */}
-                        <Button variant="contained" color="primary" style={{ marginTop: '12px' }}>
-                            Add
-                        </Button>
-                        </DialogContent>
-                    </Dialog>
-		</Grid>
-	)
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    setVisibleAdvisors(5); // Reset visible advisors when search term changes
+  };
+  return (
+    <Grid container spacing={3}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">No Advisor Found</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <TextField
+          label="Search Advisor"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <List>
+          {filteredAdvisors.slice(0, visibleAdvisors).map((advisor, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={advisor} />
+            </ListItem>
+          ))}
+        </List>
+        {visibleAdvisors < filteredAdvisors.length && (
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            align="center"
+            style={{ cursor: "pointer" }}
+            onClick={() => setVisibleAdvisors((prev) => prev + 5)}
+          >
+            Show More
+          </Typography>
+        )}
+        {filteredAdvisors.length === 0 && (
+          <Typography variant="h6" align="center" style={{ marginTop: "20px" }}>
+            No matching advisors found.
+          </Typography>
+        )}
+      </Container>
+    </Grid>
+  );
 }
 
-export default AddAdvisor
+export default AddAdvisor;
